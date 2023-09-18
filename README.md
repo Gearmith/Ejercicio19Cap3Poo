a2599f434eec3552b8df1ea2b9936239e53b6e37
johnferneymontoyaramirez <jfmontoya@ins.com.co>
2023-09-18 17:59:54 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Este es el ejercicion 19 correspondiente al capitulo 3
+
---
+++ src/main/java/john/ejercicio19cap3poo/Ejercicio19Cap3Poo.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package john.ejercicio19cap3poo;
+
+/**
+ *
+ * @author johnferneymontoyaramirez
+ */
+public class Ejercicio19Cap3Poo {
+
+    public static void main(String[] args) {
+	    Formulario form = new Formulario();
+	    form.setVisible(true);
+    }
+}
